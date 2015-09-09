92b9afe7a3aa769e2363532fe56b62ce669a453c
= <=adnan.faiz@bs-23.com>
2015-09-09 13:46:17 +0600

    defined Question model class and QuestionCategory model class

---
+++ Application/RecruitmentManagementSystem.Model/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentManagementSystem.Model
+{
+    public enum Type
+    {
+        MCQ, Descriptive
+    }
+
+    public enum DisplayType
+    {
+        Radio, ChackBox, TextArea
+    }
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }                                  
+        public Type Type { get; set; }                                      
+        public DisplayType DisplayType { get; set; }                        
+        public string Images { get; set; }                                  
+        public List<string> Answers { get; set; }                           
+        public List<string> Choices { get; set; }                           
+        public string Creator { get; set; }                                 
+        public DateTime CreatedDate { get; set; }                           
+        public string Hint { get; set; }                                    
+        public virtual QuestionCategory QuestionCategory { get; set; }      
+    }
+}
---
+++ Application/RecruitmentManagementSystem.Model/QuestionCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentManagementSystem.Model
+{
+    public enum Category
+    {
+        BasicProgramingC, ProblemSolving, OOP, Java, CSharp, DataStructures, Algorithm, Database, Analytical 
+    }
+    public class QuestionCategory
+    {
+        public int ID { get; set; }
+        public Category Category { get; set; }
+    }
+}
