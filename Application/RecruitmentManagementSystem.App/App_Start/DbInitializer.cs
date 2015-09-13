58154381770291161855e518a39305b315228dfb
Shibbir Ahmed <shibbir.io@outlook.com>
2015-09-14 00:47:08 +0600

    Add DbInitializer.cs to seed default admin

---
+++ Application/RecruitmentManagementSystem.App/App_Start/DbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RecruitmentManagementSystem.Data.DbContext;
+using RecruitmentManagementSystem.Model;
+
+namespace RecruitmentManagementSystem.App
+{
+    public class DbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            const string role = "SuperAdmin";
+            const string email = "admin-rms@bs-23.com";
+            const string password = "007@HakunaMatata";
+
+            if (!context.Users.Any(u => u.Email == email))
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+                var user = new ApplicationUser
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    UserName = email,
+                    Email = email
+                };
+
+                if (!roleManager.RoleExists(role))
+                {
+                    roleManager.Create(new IdentityRole(role));
+                }
+
+                var result = userManager.Create(user, password);
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, role);
+                }
+            }
+
+            base.Seed(context);
+        }
+    }
+}
--- Application/RecruitmentManagementSystem.App/Global.asax.cs
+++ Application/RecruitmentManagementSystem.App/Global.asax.cs
@@ -1,12 +1,7 @@
 ﻿using System.Data.Entity;
-using System.Linq;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using RecruitmentManagementSystem.Data.DbContext;
-using RecruitmentManagementSystem.Model;
 
 namespace RecruitmentManagementSystem.App
 {
@@ -18,35 +13,7 @@ namespace RecruitmentManagementSystem.App
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //Database.SetInitializer(new Init());
-        }
-    }
-
-    public class Init : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
-    {
-        protected override void Seed(ApplicationDbContext context)
-        {
-            if (!context.Users.Any(u => u.UserName == "hr@bs-23.com"))
-            {
-                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-
-                var admin = new IdentityRole {Name = "Admin"};
-                var user = new ApplicationUser
-                {
-                    UserName = "hr@bs-23.com",
-                    Email = "hr@bs-23.com",
-                    FirstName = "Adnan",
-                    LastName = "Ahmed"
-                };
-
-                userManager.Create(user, "Admin123+");
-                roleManager.Create(admin);
-
-                userManager.AddToRole(user.Id, "Admin");
-            }
-
-            base.Seed(context);
+            Database.SetInitializer(new DbInitializer());
         }
     }
 }
