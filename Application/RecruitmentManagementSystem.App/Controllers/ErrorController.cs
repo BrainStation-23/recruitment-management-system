d5bd09f520029962c34fbe8c65ef80879f8c9aca
Shibbir Ahmed <shibbir.io@outlook.com>
2015-10-11 03:54:45 +0600

    Add custom error pages for error responses

---
+++ Application/RecruitmentManagementSystem.App/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace RecruitmentManagementSystem.App.Controllers
+{
+    [Authorize]
+    public class ErrorController : Controller
+    {
+        public ActionResult PageNotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
