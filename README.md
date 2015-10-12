e150c990a6e1ef2e03e9624689343d6cc2c7845f
Shibbir Ahmed <shibbir.io@outlook.com>
2015-10-13 03:01:34 +0600

    Add xUnit for unit test

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 * [AutoMapper](http://automapper.org/)
 * [Toastr](http://codeseven.github.io/toastr/)
 * [Microsoft Unity](http://unity.codeplex.com/)
+* [xUnit](https://xunit.github.io/)
 
 # Installation
 
@@ -34,6 +35,12 @@ For more information please visit [bower.io](http://bower.io/)
 
 # License
 
-This application is released under the [MIT License.](http://www.opensource.org/licenses/MIT)
+This application is released under the [MIT](http://www.opensource.org/licenses/MIT) License.
 
-Developed By [BrainStation-23](http://brainstation-23.com/)
+Copyright (c) 2015 [BrainStation-23 Ltd.](http://brainstation-23.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- RecruitmentManagementSystem.App.Tests/Properties/AssemblyInfo.cs
+++ RecruitmentManagementSystem.App.Tests/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RecruitmentManagementSystem.App.Tests")]
@@ -14,22 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bad08fd9-60af-4d24-93d3-445b553ebbc2")]
+[assembly: Guid("ebd96b8b-cc60-46c2-917e-0efa5548d4c1")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
