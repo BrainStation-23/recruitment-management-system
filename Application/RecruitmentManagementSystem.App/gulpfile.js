87a326fa8b4cc13bf62e16f411b8fda45d900ac1
Shibbir Ahmed <shibbir.io@outlook.com>
2015-11-19 02:45:51 +0600

    Improve gulpfile.js
    Add jscs support
    Add notificaion support
    Add scss-lint support
    Update readme.md

--- Application/RecruitmentManagementSystem.App/gulpfile.js
+++ Application/RecruitmentManagementSystem.App/gulpfile.js
@@ -1,33 +1,64 @@
 ﻿var gulp = require("gulp"),
-    plugins = require("gulp-load-plugins")(),
+    plugins = require("gulp-load-plugins")({
+        lazy: true,
+        rename: {
+            "gulp-scss-lint": "scsslint"
+        }
+    }),
     runSequence = require("run-sequence");
 
 var assets = {
-    sass: "./Content/Sass/main.scss",
-    js: "./Scripts/**/*.js"
+    scss: "./Content/Sass/**/*.scss",
+    scssMain: "./Content/Sass/main.scss",
+    js: ["./Scripts/**/*.js", "!Scripts/_references.js", "gulpfile.js"]
 };
 
 gulp.task("jshint", function() {
-    return gulp.src([
-            assets.js,
-            "!Scripts/_references.js"
-        ])
+    "use strict";
+
+    return gulp.src(assets.js)
+        .pipe(plugins.print())
+        .pipe(plugins.jscs())
         .pipe(plugins.jshint())
-        .pipe(plugins.jshint.reporter("default"))
+        .pipe(plugins.jshint.reporter("jshint-stylish", { verbose: true }))
         .pipe(plugins.jshint.reporter("fail"));
 });
 
-gulp.task("sass", function() {
-    return gulp.src(assets.sass)
+gulp.task("scss-lint", function() {
+    "use strict";
+
+    return gulp.src(assets.scss)
+        .pipe(plugins.print())
+        .pipe(plugins.scsslint({
+            "config": "scss-lint.yml"
+        }))
+        .pipe(plugins.scsslint.failReporter());
+});
+
+gulp.task("scss-compile", function() {
+    "use strict";
+
+    return gulp.src(assets.scssMain)
         .pipe(plugins.sass())
-        .pipe(gulp.dest("./Content/Styles/"));
+        .pipe(gulp.dest("./Content/Styles/"))
+        .pipe(plugins.notify("SCSS files are compiled!"));
 });
 
 gulp.task("watch", function() {
+    "use strict";
+
     gulp.watch(assets.js, ["jshint"]);
-    gulp.watch(assets.sass, ["sass"]);
+    gulp.watch(assets.scss, ["scss-lint", "scss-compile"]);
+});
+
+gulp.task("lint", function(done) {
+    "use strict";
+
+    runSequence("scss-lint", "jshint", done);
 });
 
 gulp.task("default", function(done) {
-    runSequence("jshint", "sass", ["watch"], done);
+    "use strict";
+
+    runSequence("lint", "scss-compile", ["watch"], done);
 });
